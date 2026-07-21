458529c65a890d054cfbbcc85573a67f0a438c29
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-21 13:44:00 -0700

    test: correct domain architecture test reference

--- tests/AlexaCloudAudio.Domain.Tests/ArchitectureTests.cs
+++ tests/AlexaCloudAudio.Domain.Tests/ArchitectureTests.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using Xunit;
 
 namespace AlexaCloudAudio.Domain.Tests;
@@ -8,7 +7,7 @@ public sealed class ArchitectureTests
     [Fact]
     public void Domain_does_not_reference_other_solution_projects()
     {
-        var references = typeof(Domain.AssemblyMarker).Assembly
+        var references = typeof(global::AlexaCloudAudio.Domain.AssemblyMarker).Assembly
             .GetReferencedAssemblies()
             .Select(static assembly => assembly.Name)
             .Where(static name => name is not null && name.StartsWith("AlexaCloudAudio.", StringComparison.Ordinal))
